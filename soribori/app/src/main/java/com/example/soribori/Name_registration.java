8bd498be21b4868113331ace5b5b5757b06c5359
HunMinKim <gnsals0904@ajou.ac.kr>
2020-02-01 19:03:24 +0900

    add Name TextView and if name registration success, send toast msg to user (using try catch)

--- soribori/app/src/main/java/com/example/soribori/Name_registration.java
+++ soribori/app/src/main/java/com/example/soribori/Name_registration.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kakao.sdk.newtoneapi.SpeechRecognizerManager;
 
 public class Name_registration extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = Name_registration.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +35,19 @@ public class Name_registration extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         TextView textView2 = (TextView)findViewById(R.id.textView4);
 
-        // textview 에 있는  string 을 UserName 에 넣어
-        String UserName = textView2.getText().toString();
+        try {
+            // textview 에 있는  string 을 UserName 에 넣어
+            String UserName = textView2.getText().toString();
 
-        // 인텐트를 만들어서 string을 username이라는 이름으로 던져줘
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.putExtra("UserName",UserName);
+            // 인텐트를 만들어서 string을 username이라는 이름으로 던져줘
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("UserName", UserName);
 
+            // User에게 토스트메세지로 알림
+            Toast.makeText(getApplicationContext(), "이름 등록이 완료 되었습니다.", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Log.d(TAG,"Name Registration Error!!\n",e);
+        }
     }
 
     @Override
