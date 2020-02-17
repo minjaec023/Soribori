c576c6c72e071a6a52a32c39f7532678417da28e
HunMinKim <gnsals0904@ajou.ac.kr>
2020-02-17 14:34:04 +0900

    complete hiding develop button

--- soribori/app/src/main/java/com/example/soribori/Recording.java
+++ soribori/app/src/main/java/com/example/soribori/Recording.java
@@ -2,10 +2,15 @@ package com.example.soribori;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Recording extends ToolbarActivity{
+
+    int bt_develop_count = 0;
+
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
@@ -22,8 +27,21 @@ public class Recording extends ToolbarActivity{
         bt_develop.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+
+                if(bt_develop_count < 5){
+                    final Toast toast = Toast.makeText(getApplicationContext(), "개발자 페이지에 접근하려면 "+ (5 - bt_develop_count)+"번 더 연속으로 클릭하세요", Toast.LENGTH_SHORT);
+                    toast.show();
+                    bt_develop_count++;
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override public void run() {
+                            toast.cancel(); } }, 200);
+
+                }
+                else if(bt_develop_count == 5) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
