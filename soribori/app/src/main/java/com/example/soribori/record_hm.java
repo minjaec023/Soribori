125c44fe43a685497458cdc0961a6feb4e421c96
HunMinKim <gnsals0904@ajou.ac.kr>
2020-05-10 21:35:42 +0900

    start ui design

---
+++ soribori/app/src/main/java/com/example/soribori/record_hm.java
@@ -0,0 +1,14 @@
+package com.example.soribori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class record_hm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_hm);
+    }
+}
